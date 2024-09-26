ed5dc6cf6e7f59ba940ba7906c4976da4b8b3b52
weibinke <283142081@qq.com>
2024-09-26 20:24:19 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,26 @@
+# -*- coding:utf8 -*-
+# URL=: | https://gesso.com
+# Import the required libraries
+from .request import(
+    Text,
+    DataTextbooks,
+    get_object,
+    write,
+    DataTextbooks
+)
+
+from .request.DataTextbooks import(
+    coding
+)
+
+
+__all__ = [
+    "Text",
+    "DataTextbooks",
+    "get_object",
+    "write",
+    "DataTextbooks",
+    "coding"
+]
+__author__ = "weibinke"
+__version__ = "0.0.1"
---
+++ __init__.pyi
@@ -0,0 +1,4 @@
+from os import path as p
+from sys import path as p2
+
+p2.append(p.abspath("gesso"))
